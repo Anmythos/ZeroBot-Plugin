0090954df090c63599968434cd2b0d2e3be89a22
Anmythos <Anmythos@hotmail.com>
2025-03-25 14:22:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ import (
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chat" // 基础词库
 
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chatcount" // 聊天时长统计
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chatcount" // 聊天时长统计2
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/sleepmanage" // 统计睡眠时间
 
